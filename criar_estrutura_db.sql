0d6612f2b99e13a68f3fa76f67f621d221c8c714
Cyntia Beatriz <cyntiacabral32@gmail.com>
2023-07-21 14:36:01 -0300

    Atualizando arquivo Sql

--- criar_estrutura_db.sql
+++ criar_estrutura_db.sql
@@ -5,50 +5,50 @@ CREATE TABLE "SERVIDORES"(
     "id_setor_siape" SERIAL NOT NULL,
     "id_disciplina_ingresso" SERIAL NOT NULL,
     "id_setor_suap" SERIAL NOT NULL,
-    "nome" VARCHAR(255) NOT NULL,
-    "funcao" VARCHAR(255) NOT NULL,
-    "jornada_trabalho" CHAR(255) NOT NULL,
+    "nome" VARCHAR(50) NOT NULL,
+    "funcao" VARCHAR(30) NOT NULL,
+    "jornada_trabalho" CHAR(20) NOT NULL,
     "telefones_institucionais" BIGINT NOT NULL,
-    "curriculo_lattes" CHAR(255) NOT NULL,
-    "campus" VARCHAR(255) NOT NULL,
+    "curriculo_lattes" CHAR(150) NOT NULL,
+    "campus" VARCHAR(20) NOT NULL,
     "url_foto_75x100" VARCHAR(255) NOT NULL
 );
 ALTER TABLE
     "SERVIDORES" ADD PRIMARY KEY("matricula");
 CREATE TABLE "CATEGORIA_SERVIDORES"(
     "id" SERIAL NOT NULL,
-    "categoria" CHAR(255) NOT NULL
+    "categoria" CHAR(20) NOT NULL
 );
 ALTER TABLE
     "CATEGORIA_SERVIDORES" ADD PRIMARY KEY("id");
 CREATE TABLE "CAMPI"(
-    "campus" VARCHAR(255) NOT NULL,
-    "nome_completo" VARCHAR(255) NOT NULL
+    "campus" VARCHAR(20) NOT NULL,
+    "nome_completo" VARCHAR(50) NOT NULL
 );
 ALTER TABLE
     "CAMPI" ADD PRIMARY KEY("campus");
 CREATE TABLE "DISCIPLINA_INGRESSO"(
     "id" SERIAL NOT NULL,
-    "disciplina" VARCHAR(255) NOT NULL
+    "disciplina" VARCHAR(60) NOT NULL
 );
 ALTER TABLE
     "DISCIPLINA_INGRESSO" ADD PRIMARY KEY("id");
 CREATE TABLE "SETORES_SIAPE"(
     "id" SERIAL NOT NULL,
-    "nome_setor" CHAR(255) NOT NULL
+    "nome_setor" CHAR(25) NOT NULL
 );
 ALTER TABLE
     "SETORES_SIAPE" ADD PRIMARY KEY("id");
 CREATE TABLE "CARGO_SERVIDORES"(
     "id" SERIAL NOT NULL,
-    "cargo" VARCHAR(255) NOT NULL
+    "cargo" VARCHAR(30) NOT NULL
 );
 ALTER TABLE
     "CARGO_SERVIDORES" ADD PRIMARY KEY("id");
 
 CREATE TABLE "SETORES_SUAP"(
     "id" SERIAL NOT NULL,
-    "nome_setor" CHAR(255) NOT NULL
+    "nome_setor" CHAR(35) NOT NULL
 );
 ALTER TABLE
     "SETORES_SUAP" ADD PRIMARY KEY("id");
