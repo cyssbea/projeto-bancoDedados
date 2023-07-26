a06b721de5b7e85410e1ed10f9d50a8e1146204f
Cyntia Beatriz <cyntiacabral32@gmail.com>
2023-07-26 10:23:15 -0300

    Atualizando db

--- criar_estrutura_db.sql
+++ criar_estrutura_db.sql
@@ -1,3 +1,17 @@
+-- Database: ifrn_servidores
+
+-- DROP DATABASE IF EXISTS ifrn_servidores;
+
+CREATE DATABASE ifrn_servidores
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
 CREATE TABLE "SERVIDORES"(
     "matricula" BOOLEAN NOT NULL,
     "id_categoria_servidores" SERIAL NOT NULL,
